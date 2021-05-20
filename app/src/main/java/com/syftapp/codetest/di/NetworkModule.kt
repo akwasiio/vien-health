48e9093968b2b143d0765e57b13cba7e30c19c62
oppongjr <oppong751@gmail.com>
2021-05-20 02:39:58 +0000

    Added httpinterceptor

--- app/src/main/java/com/syftapp/codetest/di/NetworkModule.kt
+++ app/src/main/java/com/syftapp/codetest/di/NetworkModule.kt
@@ -3,16 +3,20 @@ package com.syftapp.codetest.di
 import com.syftapp.codetest.BuildConfig
 import com.syftapp.codetest.data.api.BlogApi
 import com.syftapp.codetest.data.api.BlogService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val networkModule = module {
 
     single {
         Retrofit.Builder()
             .baseUrl(BuildConfig.API_ROOT)
+            .client(httpClient())
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
@@ -22,4 +26,18 @@ val networkModule = module {
 
     single { BlogApi(get()) }
 
+}
+
+private fun httpClient(): OkHttpClient {
+
+    val httpLoggingInterceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger.DEFAULT)
+    val clientBuilder = OkHttpClient.Builder()
+    if (BuildConfig.DEBUG) {
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        clientBuilder.addInterceptor(httpLoggingInterceptor)
+    }
+
+    clientBuilder.readTimeout(120, TimeUnit.SECONDS)
+    clientBuilder.writeTimeout(120, TimeUnit.SECONDS)
+    return clientBuilder.build()
 }
