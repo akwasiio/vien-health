785f21a9ed19d52065f84c23a816d623f69185d9
oppongjr <oppong751@gmail.com>
2021-05-20 13:28:44 +0000

    Refactored BlogRepository constructor

--- app/src/main/java/com/syftapp/codetest/di/DataModule.kt
+++ app/src/main/java/com/syftapp/codetest/di/DataModule.kt
@@ -6,12 +6,8 @@ import org.koin.dsl.module
 
 val dataModule = module {
 
-    single { BlogRepository(get(), get(), get(), get(), get(), get()) }
+    single { BlogRepository(get(), get(),) }
 
     single {AppDatabase(get())}
-    single { AppDatabase(get()).commentDao() }
-    single { AppDatabase(get()).postDao() }
-    single {AppDatabase(get()).postKeyDao()}
-    single { AppDatabase(get()).userDao() }
 
 }
